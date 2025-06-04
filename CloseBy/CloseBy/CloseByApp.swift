b0997c4b0e73efa4a37d88599dcbe723306536c8
Omer Oguz Celikel <omeroguzcelikel@gmail.com>
2025-06-04 23:11:00 +0300

    app is created

---
+++ CloseBy/CloseBy/CloseByApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloseByApp.swift
+//  CloseBy
+//
+//  Created by Omer Oguz Celikel on 4.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct CloseByApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
